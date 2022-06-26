0cd9ab5cbdc8a7714516004605af1c4030f99425
Jurgis <jurgisp@gmail.com>
2022-06-26 10:42:27 +0300

    Remove python 3.8 requirement

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 setup(
     name="dmc-memory-maze",
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/jurgisp/dmc-memory-maze",
     description="Python wrapper for DMLab maze generator",
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3",
     packages=["dmc_memory_maze"],
     install_requires=[
         'dm_control'
